583ec6fbe5216f600d1b7e610e41eca4dda7469c
Codigowww <git@codigowww.com>
2025-04-13 06:45:53 -0300

    chars problem

--- lavadora.ino
+++ lavadora.ino
@@ -480,13 +480,15 @@ void loopLavadora()
 
 void processCommand(String input)
 {
+  char inputChars[] = input.c_str();
 
-  JSONVar myObject = JSON.parse(input);
+  JSONVar myObject = JSON.parse(inputChars);
 
   // JSON.typeof(jsonVar) can be used to get the type of the variable
   if (JSON.typeof(myObject) == "undefined")
   {
     logMessage("{\"error\":\"Invalid JSON code 1\"}");
+    logMessage(input);
     return;
   }
 
